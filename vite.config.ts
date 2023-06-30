cba340064b9a30292158ef0926510f4af7b4646a
Kevin Traini <kevin@ktraini.com>
2023-06-30 17:49:52 +0200

    Add vitest and coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import {resolve} from 'node:path'
-import {defineConfig} from 'vite'
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     build: {
         lib: {
-            entry: [
-                resolve(__dirname, "src/main.ts"),
-            ],
+            entry: [resolve(__dirname, 'src/main.ts')],
             name: 'Freddy',
-            formats: ['es']
+            formats: ['es'],
         },
         target: 'modules',
-    }
+    },
+    test: {
+        include: ['tests/**/*.test.ts'],
+        watch: false,
+        coverage: {
+            provider: 'v8',
+            include: ['src/*.ts'],
+            exclude: ['tests/**/*.test.ts'],
+            all: true,
+        },
+    },
 });
